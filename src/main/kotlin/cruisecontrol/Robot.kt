4df718612280f5638481a78ac2c8dca64de2e42e
brendon-ai <brendon-m@outlook.com>
2017-12-17 15:59:10 -0500

    Initial commit

---
+++ src/main/kotlin/cruisecontrol/Robot.kt
@@ -0,0 +1,12 @@
+package cruisecontrol
+
+import edu.wpi.first.wpilibj.IterativeRobot
+
+// The main robot class
+class Robot : IterativeRobot() {
+
+    // Called at the beginning of autonomous mode (the car should always be in autonomous mode)
+    override fun autonomousInit() {
+
+    }
+}
